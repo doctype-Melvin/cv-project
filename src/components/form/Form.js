f10f629dfc3a7a6da3e51735525c46a4f7184227
doctype-Melvin <doctype.melvin@gmail.com>
2022-12-09 13:49:41 +0100

    <RE>adds dynamic data output - just a test

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -21,6 +21,8 @@ export class Form extends React.Component {
     render() {
         return (
             <div className="formCt">
+                {/* Below is an example of how to output dynamic data */}
+                { 'This message comes from Form.js render'}
                 <form className="formEl" onSubmit={this.handleSubmit}>
                         <label>Name:
                         <input type="text" className="input" id="name" value={this.state.value} onChange={this.handleInput} />
